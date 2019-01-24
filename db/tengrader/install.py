159313100af4c44216f07f2902c4846b89d623c7
Robinson <rj.rosag@gmail.com>
2019-01-23 22:16:03 -0500

    v1.1-0 Install Cassandra connection & logging"

--- db/tengrader/install.py
+++ db/tengrader/install.py
@@ -1,9 +1,32 @@
+import logging
 from cassandra import ConsistencyLevel
+from cassandra.cluster import Cluster, BatchStatement
+from cassandra.query import SimpleStatement
 
 class TengraderInstall:
 	def __init__(self, home=""):
 		self.home = home 
-		print(self.home )
+		self.cluster = None
+		self.session = None
+		self.keyspace = None
+		self.log = None
+
+	def createsession(self):
+		self.cluster = Cluster(['localhost'])
+		self.session = self.cluster.connect(self.keyspace)
+
+	def setlogger(self):
+		log = logging.getLogger()
+		log.setLevel('INFO')
+		handler = logging.StreamHandler()
+		handler.setFormatter(logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(messages)s"))
+		log.addHandler(handler)
+		self.log = log
+
+	def getsession(self):
+		return (self.session)
+
+
 	# Directory structure Add
 	# DB Add 
 	# Default value provision
@@ -12,5 +35,12 @@ class TengraderInstall:
 if __name__ == "__main__":
 	home = "/home/drosa/dev/db/tengrader"
 	Installer = TengraderInstall(home)
+	Installer.createsession()
+	Installer.setlogger()
+	'''
+	References: 
+	URL:
+		https://techfossguru.com/apache-cassandra-python-step-step-guide-ubuntu-example/
+	'''
 
 
