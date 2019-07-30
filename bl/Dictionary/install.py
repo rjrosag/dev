e44df45bb7e79dbe45fbbfd2254043719e391392
rj.rosag@gmail.com <rj.rosag@gmail.com>
2019-07-29 21:53:45 -0400

    v0.3 Inserting data into attribute table

--- bl/Dictionary/install.py
+++ bl/Dictionary/install.py
@@ -18,14 +18,15 @@ class dictionarydb:
     cursor = mydb.cursor(buffered=None)
     cursor.execute("CREATE DATABASE IF NOT EXISTS dictionary;")
     cursor.execute("USE dictionary;")
-    cursor.execute("DROP TABLE IF EXISTS Attribute;")
-    sql_command = '''CREATE TABLE IF NOT EXISTS Attribute (
-                            id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    cursor.execute("DROP TABLE IF EXISTS attribute;")
+    sql_command = '''CREATE TABLE IF NOT EXISTS attribute (
+                            attributeid INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
                             sys VARCHAR(3),
                             tablename VARCHAR(10), 
                             name VARCHAR(30),
-                            type VARCHAR(10),
+                            type VARCHAR(50),
                             length INT,
+                            _decimal INT,
                             format VARCHAR(15), 
                             INDEX NAME (sys, tablename)                            
                             );
@@ -35,7 +36,47 @@ class dictionarydb:
     mydb.commit()
     mydb.close()
 
+  def put(self, table = {}):
+      mydb = mysql.connector.connect(
+          host=os.environ.get('DICTIONARY_DBHOST', "localhost"),
+          user=os.environ.get('DICTIONARY_DBUSER', 'admin'),
+          passwd=os.environ.get('DICTIONARY_DBPASSWORD', 'c0mcast!'))
+      cursor = mydb.cursor(buffered=None)
+      cursor.execute("USE dictionary;")
+      for record in table:
+          if 'INT' in record:
+              sql_command = 'INSERT INTO attribute (sys,tablename,name,type,length,_decimal) VALUES ('+ record +');'
+          elif 'DATE' in record:
+              sql_command = 'INSERT INTO attribute (sys,tablename,name,type) VALUES (' + record + ');'
+          else:
+              sql_command = 'INSERT INTO attribute (sys,tablename,name,type,length) VALUES ('+ record +');'
+          cursor.execute(sql_command)
+      cursor.close()
+      mydb.commit()
+      mydb.close()
 
 
-sysdb = dictionarydb()
-sysdb.create()
+
+
+
+dict = dictionarydb()
+dict.create()
+sql_table = {'"scf", "catalog", "id", "INT NOT NULL PRIMARY KEY AUTO_INCREMENT", 10, 0',
+              '"scf", "catalog", "name", "CHAR", 30',
+              '"scf", "catalog", "control", "INT", 15, 0',
+              '"scf", "catalog", "debit", "INT",  15, 2',
+              '"scf", "catalog", "credit", "INT", 15, 2',
+              '"scf", "catalog", "status", "CHAR", 10'}
+dict.put(sql_table)
+
+sql_table = {'"scf", "cycle", "id", "INT NOT NULL PRIMARY KEY AUTO_INCREMENT", 10, 0',
+              '"scf", "cycle", "name", "CHAR", 30',
+              '"scf", "cycle", "begindate", "DATE"',
+              '"scf", "cycle", "enddate", "DATE"',
+              '"scf", "cycle", "type", "CHAR", 15' ,
+              '"scf", "cycle", "recurrency", "CHAR", 10',
+              '"scf", "cycle", "priority", "INT", 3, 0',
+              '"scf", "cycle", "status", "CHAR", 10'
+             }
+dict.put(sql_table)
+
