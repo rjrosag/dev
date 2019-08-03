b48553bb61f310378cce9992c69d37191d2dbd17
rj.rosag@gmail.com <rj.rosag@gmail.com>
2019-08-03 17:01:50 -0400

    v0.8 Express forms + Add/update/delete in array

--- db/nodejs/demo/index.js
+++ db/nodejs/demo/index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const path = require('path');
 const moment = require('moment');
 const logger = require('./middleware/logger');
+const exphbs = require('express-handlebars');
+const members = require('./Members');
 
 // Init express
 const app = express();
 
+// Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 const PORT= process.env.PORT || 5000;
 
@@ -15,6 +20,13 @@ app.use(logger);
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
+
+// Homepage route
+app.get('/', (req, res) => res.render('index', {
+   title: 'Member App', 
+   members
+}));
+
 
 // Listen on port
 app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
--- db/nodejs/demo/routes/api/members.js
+++ db/nodejs/demo/routes/api/members.js
@@ -17,7 +17,7 @@ router.get('/:id', (req, res) => {
     } else {
        res.status(404).json({ msg: `No member with the id: ${req.params.id}`});
     }
-})
+});
 
 // Create member
 router.post('/', (req, res) => {
@@ -32,6 +32,7 @@ router.post('/', (req, res) => {
     } 
     members.push(newMember);
     res.json(members); 
+    // res.redirect('/');
 });
 
 // Update member
@@ -44,23 +45,23 @@ router.put('/:id', (req, res) => {
              member.name = updMember.name ? updMember.name : member.name;
              member.email = updMember.email ? updMember.email : member.email;
           }
-       } 
+       }); 
     } else {
-        res.status(404).json({msg: 'No member with the id of ${req.params.id}'});
+        res.status(404).json({msg: `No member with the id of ${req.params.id}`});
     }
 });
-A
-A
-B
-A
-B
-A
-B
-B
-B
-A
-});
 
+// Delete a member
+router.delete('/:id', (req, res) => {
+   const found = members.some(member => member.id === parseInt(req.params.id));
+   if (found) {
+      res.json({msg: 'Member delete',  
+               members: members.filter(member => member.id !== parseInt(req.params.id))
+              });
+   } else {
+      res.status(404).json({msg: `No member with the id of ${req.params.id}`});
+   }
+});
 module.exports=router 
 
 
