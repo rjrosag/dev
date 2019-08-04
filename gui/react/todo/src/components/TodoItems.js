05379c91d01fcf925332acaa15b4e3b77c287e9b
rj.rosag@gmail.com <rj.rosag@gmail.com>
2019-08-04 19:56:10 -0400

    v1.0 Express GUI Styling

--- gui/react/todo/src/components/TodoItems.js
+++ gui/react/todo/src/components/TodoItems.js
@@ -1,14 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
-
+   getStyle = () => {
+       return {
+          textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+       }
+   }
    render() {
       return (
-         <div>
-              <p>Hello</p>
+         <div style = {this.getStyle() }>
+              <p>{ this.props.todo.title }</p>
          </div>
       )
    }
 }
 
+// Property type
+//TodoItem.PropTypes = {
+//   todo: PropTypes.object.isRequired
+//}
 export default TodoItem;
